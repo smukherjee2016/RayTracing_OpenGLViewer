40ea940ea193b07759003ccfc0e8616b4e5713a5
Sabyasachi Mukherjee <sabyasachimukherjee2006@gmail.com>
2018-08-27 01:08:30 +0900

    Add required header files

--- include/RayTracing_OpenGLViewer.hpp
+++ include/RayTracing_OpenGLViewer.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include <iostream>
+
+// Include GLEW. Include it before gl.h and glfw3.h.
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
