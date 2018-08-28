7ece52daf61212e58ddb91ad3b39d04886faed6e
Sabyasachi Mukherjee <smukherjee2016@users.noreply.github.com>
2018-08-28 20:33:49 +0900

    Move shader into a separate class and have a covering triangle for textures

--- include/RayTracing_OpenGLViewer.hpp
+++ include/RayTracing_OpenGLViewer.hpp
@@ -18,15 +18,18 @@ static thread_local std::uniform_real_distribution<double> rando(-1.0f, 1.0f);
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"vertexColor = vec4(1.0 - (aPos.x + 1.0)/2.0,1.0 - (aPos.y + 1.0)/2.0, 0.0, 1.0);\n"
 "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 class Shader
